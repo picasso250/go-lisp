637054bf6a8b400d525c8a28a92b446c77ef139d
pica <cumt.xiaochi@gmail.com>
2026-03-17 13:33:23 +0800

    test: ensure all built-in functions have corresponding E2E tests

--- tests/math_conv.lisp
+++ tests/math_conv.lisp
@@ -1,2 +1,2 @@
-(abs -50) (pow 2 10) (str 123) (int "456") (bool 0) (bool 1) (bool '())
-; EXPECT ["50", "1024", "123", "456", "false", "true", "false"]
+(abs -50) (pow 2 10) (str 123) (int "456") (integer 789) (bool 0) (bool 1) (bool '()) (divmod 10 3) (round 3.6) (round 3.4)
+; EXPECT ["50", "1024", "123", "456", "789", "false", "true", "false", "[3 1]", "4", "3"]
---
+++ tests/print.lisp
@@ -0,0 +1,2 @@
+(print "hello" 1 2)
+; EXPECT ["hello 1 2"]
